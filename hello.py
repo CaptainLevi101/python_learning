dcd6ef4cae047471bde6fca796f4240686027ee7
Ashish Parashar <200104016@hbtu.ac.in>
2024-07-26 20:12:48 +0530

    add first Program

---
+++ hello.py
@@ -0,0 +1,9 @@
+print("Hello world!")
+
+#inner working of python
+#python source code is converted into byte code
+# byte code is a low level code which is platform independent
+# byte code runs faster than scripts because of less operations
+# .pyc is a compiled python
+# __pycache__ used for internal working in python
+# diffing algorithms are used in python like reactJs
